d54bf04c6dc0c62021224e2b98fdbe7dd13d871e
dmstreaming2 <mallmadaniel7@gmail.com>
2026-01-12 22:55:39 -0500

    Create app.js

---
+++ assets/js/app.js
@@ -0,0 +1,8 @@
+// año automático
+document.getElementById('y').textContent = new Date().getFullYear();
+
+// aquí puedes agregar:
+// - botón copiar
+// - animaciones
+// - efectos visuales
+// SIN tocar freegen()
